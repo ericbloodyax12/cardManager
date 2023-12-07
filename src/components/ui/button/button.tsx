95f21349e48cda47f9c94fff499bc1bf58352893
Young Eric <erikbonopart12@gmail.com>
2023-12-07 18:37:35 +0300

    -add js doc (comments and descriptions)

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -3,11 +3,15 @@ import { ComponentPropsWithoutRef } from 'react'
 import s from './button.module.scss'
 
 export type ButtonProps = {
+    /**
+     * add width: 100% to the 'button' el */
     variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
     fullWidth?: boolean
 } & ComponentPropsWithoutRef<'button'> //это пропсы, которые принимает стандартный html-тег button, мы их расширяем своими пропсами
-
+/**
+ * js doc - Button component description */
 export const Button = ({ variant = 'primary', fullWidth, className, ...rest }: ButtonProps) => {
+
     return (
         <button className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
     )
