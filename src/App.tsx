8eda3ac44a4813dabc15d16ea51dce49599f4295
George <7GBars@gmail.com>
2023-12-17 01:17:49 +0300

    -рефактор файла стилей для типографии:
    добавить миксин для общих стилей
    перевести размер шрифта на rem от 10px html

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 export function App() {
   return (
-    <div>
-      <Button
-        as={'a'}
-        href={
-          'https://www.youtube.com/watch?v=8D9d9weVQnI&list=PLqFTZCcUCeaQ0e9QmIKVrAGEWFrIWR4nm&index=8'
-        }
-      >
-        sdsdsd
-      </Button>
-      <Button>
-        <Typography variant={'body1'}>button2</Typography>
-      </Button>
+    <div style={{ width: '100px' }}>
+      <Typography as={'a'} variant={'subtitle2'}>
+        Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH
+      </Typography>
+      <br />
     </div>
   )
 }
