5a4a6c8fb81b94b995a85698e0bc9e80a55140a6
Young Eric <erikbonopart12@gmail.com>
2023-12-23 13:18:27 +0300

    -fix bugs for CheckboxComponent -set styles for CheckboxComponent -add common radix Checkbox props

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { CheckboxComponent } from '@/components/ui/ checkbox'
 import { Typography } from '@/components/ui/typography'
 export function App() {
   return (
-    <div style={{ width: '100px' }}>
+    <div>
       <Typography as={'a'}>
         Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH
       </Typography>
-      <br />
-      <CheckboxComponent />
+      <CheckboxComponent withLabel />
     </div>
   )
 }
--- src/components/ui/ checkbox/checkbox.tsx
+++ src/components/ui/ checkbox/checkbox.tsx
@@ -3,37 +3,37 @@ import { useState } from 'react'
 import { CheckIcon } from '@/components/assets/icons/componentSvg/check'
 import { Typography } from '@/components/ui/typography'
 import * as Checkbox from '@radix-ui/react-checkbox'
+import { CheckboxProps } from '@radix-ui/react-checkbox'
 
 import s from './checkbox.module.scss'
 
 type CheckboxComponentProps = {
   withLabel?: boolean
-}
+} & CheckboxProps
 export const CheckboxComponent = (props: CheckboxComponentProps) => {
-  const [checked, setChecked] = useState<'indeterminate' | boolean>(false)
-  const { withLabel = false } = props
+  const [checked, setChecked] = useState<boolean>(false)
+  const { withLabel = false, ...rest } = props
 
   return (
-    <form>
-      <div>
-        <Checkbox.Root
-          checked={checked}
-          className={s.CheckboxRoot}
-          defaultChecked
-          id={'c1'}
-          onCheckedChange={setChecked}
-        >
-          <Checkbox.Indicator className={s.CheckboxIndicator} forceMount>
-            {/* forceMount свойство, которое не дает исчезать после checked, нужно до конца разобраться потому что сейчас и без него работает  */}
-            {checked === true ? <CheckIcon /> : ''}
-          </Checkbox.Indicator>
-        </Checkbox.Root>
-        {withLabel && (
-          <label className={'s.Label'} htmlFor={'c1'}>
-            <Typography>Accept terms and conditions.</Typography>
-          </label>
-        )}
-      </div>
-    </form>
+    <div className={s.checkboxContainer}>
+      <Checkbox.Root
+        checked={checked}
+        className={s.CheckboxRoot}
+        id={'c1'}
+        onCheckedChange={setChecked as (checked: 'indeterminate' | boolean) => void}
+        {...rest}
+      >
+        <Checkbox.Indicator className={s.CheckboxIndicator}>
+          {checked === true ? <CheckIcon /> : ''}
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      {withLabel && (
+        <label className={s.Label} htmlFor={'c1'}>
+          <Typography as={'p'} variant={'body2'}>
+            Accept terms and conditions.
+          </Typography>
+        </label>
+      )}
+    </div>
   )
 }
