a4cc3a1be14382f58e3c77be7fdd90a35215d36d
Young Eric <erikbonopart12@gmail.com>
2024-02-07 18:40:40 +0300

    -refactor -add react-router-dom library

---
+++ src/components/auth/login/helpers/loginSchema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+const emailSchema = z.string().trim().email() // required by default
+const passwordSchema = z.string().min(3) // required by default
+const rememberMeSchema = z.boolean().default(false) // required by default
+
+export const loginSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+  rememberMe: rememberMeSchema,
+})
--- src/components/ui/ checkbox/controlled-checkbox.tsx
+++
@@ -1,9 +0,0 @@
-// import {useController} from "react-hook-form";
-//
-//
-// export const ControlledCheckbox =
-//
-//
-// const {
-//     field: { onChange, value },
-// } = useController({ control, defaultValue: false, name: 'rememberMe' })
