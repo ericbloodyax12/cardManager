f046a069cf9a0d743b6b6a238489088a608c41e9
Young Eric <erikbonopart12@gmail.com>
2024-02-23 14:23:21 +0300

    - add test todo auth-services -add helper setAsyncTodos

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,13 @@ import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 
+
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+   </React.StrictMode>
 )
