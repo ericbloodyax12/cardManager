b6095b6f847a8ff697760fd7d5b1ea04b1410c08
Young Eric <erikbonopart12@gmail.com>
2024-03-05 20:21:00 +0300

    -refactor: rewrite authServices into the class

--- src/services/api/auth-services.ts
+++ src/services/api/auth-services.ts
@@ -1,56 +1,77 @@
-import {AuthResponseType, CreateUserResponseType, DecksResponseType} from "@/services/api/authTypes";
+import {AuthResponseType, CreateUserResponseType} from "@/services/api/authTypes";
 
-export type TodosType = {
-  "userId": number,
-  "id": number,
-  "title": string,
-  "completed": boolean
-}
+class AuthServices {
+  readonly baseUrl = 'https://api.flashcards.andrii.es'
 
-export const authServices = {
-  baseUrl: 'https://api.flashcards.andrii.es',
-
-  getTodos: async (): Promise<TodosType[]> => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const todos = await res.json()
-    return todos
-  },
-
-  getDecks: (): Promise<DecksResponseType> => {
-    const decksPath = '/v1/decks'
-    return fetch(authServices.baseUrl + decksPath, {
-      headers: {'x-auth-skip': 'true'}
-    })
-        .then((res) => res.json())
-        .then((data) => {
-          return data
-        })
-  },
-
-  getIsAuth: async (): Promise<AuthResponseType> => {
+  async getIsAuth(): Promise<AuthResponseType> {
     const authPath = '/v1/auth/me'
-    const res = await fetch(authServices.baseUrl + authPath)
+    const res = await fetch(this.baseUrl + authPath)
     return (res.status === 200)
         ? await res.json()
         : await res.json()
-  },
+  }
 
-  postCreateUser: async (email:string, password:string): Promise<CreateUserResponseType> => {
-      const signUpPath = '/v1/auth/sign-up'
-      const response = await fetch(authServices.baseUrl + signUpPath, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json;charset=utf-8'},
-        body: JSON.stringify({email, password})
-      });
-      if (response.status !== 201) {
-        const errorData = await response.json()
-        const errorMessage = errorData.errorMessages[0]
-        throw new Error(errorMessage)
-      }
-      return await response.json()
+  async postCreateUser(email: string, password: string): Promise<CreateUserResponseType> {
+    const signUpPath = '/v1/auth/sign-up'
+    const response = await fetch(this.baseUrl + signUpPath, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json;charset=utf-8'},
+      body: JSON.stringify({email, password})
+    });
+    if (response.status !== 201) {
+      const errorData = await response.json()
+      const errorMessage = errorData.errorMessages[0]
+      throw new Error(errorMessage)
     }
+    return await response.json()
+  }
 
-} as const
+} //как сделать аналог as const для классов
+export const authServices = new AuthServices()
+// export const authServices = {
+//   baseUrl: 'https://api.flashcards.andrii.es',
+//
+//   getTodos: async (): Promise<TodosType[]> => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+//     const todos = await res.json()
+//     return todos
+//   },
+//
+//   getDecks: (): Promise<DecksResponseType> => {
+//     const decksPath = '/v1/decks'
+//     return fetch(authServices.baseUrl + decksPath, {
+//       headers: {'x-auth-skip': 'true'}
+//     })
+//         .then((res) => res.json())
+//         .then((data) => {
+//           return data
+//         })
+//   },
+//
+//   getIsAuth: async (): Promise<AuthResponseType> => {
+//     const authPath = '/v1/auth/me'
+//     const res = await fetch(authServices.baseUrl + authPath)
+//     return (res.status === 200)
+//         ? await res.json()
+//         : await res.json()
+//   },
+//
+//   postCreateUser: async (email:string, password:string): Promise<CreateUserResponseType> => {
+//       const signUpPath = '/v1/auth/sign-up'
+//       const response = await fetch(authServices.baseUrl + signUpPath, {
+//         method: 'POST',
+//         headers: {'Content-Type': 'application/json;charset=utf-8'},
+//         body: JSON.stringify({email, password})
+//       });
+//       if (response.status !== 201) {
+//         const errorData = await response.json()
+//         const errorMessage = errorData.errorMessages[0]
+//         throw new Error(errorMessage)
+//       }
+//       return await response.json()
+//     }
+//
+// } as const
 
 
 // const baseUrl = 'https://api.flashcards.andrii.es'
