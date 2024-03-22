d3e17ff0dbb2d7dd55c24cc2cd3069d37725d606
Young Eric <erikbonopart12@gmail.com>
2024-03-22 19:36:28 +0300

    -fix checkbox bugs

--- src/services/api/auth-services.ts
+++ src/services/api/auth-services.ts
@@ -1,7 +1,7 @@
 import {AuthResponseType, CreateUserResponseType} from "@/services/api/authTypes";
 
 class AuthServices {
-  readonly baseUrl = 'https://api.flashcards.andrii.es'
+  private readonly baseUrl = 'https://api.flashcards.andrii.es'
 
   async getIsAuth(): Promise<AuthResponseType> {
     const authPath = '/v1/auth/me'
@@ -28,73 +28,7 @@ class AuthServices {
 
 } //как сделать аналог as const для классов
 export const authServices = new AuthServices()
-// export const authServices = {
-//   baseUrl: 'https://api.flashcards.andrii.es',
-//
-//   getTodos: async (): Promise<TodosType[]> => {
-//     const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-//     const todos = await res.json()
-//     return todos
-//   },
-//
-//   getDecks: (): Promise<DecksResponseType> => {
-//     const decksPath = '/v1/decks'
-//     return fetch(authServices.baseUrl + decksPath, {
-//       headers: {'x-auth-skip': 'true'}
-//     })
-//         .then((res) => res.json())
-//         .then((data) => {
-//           return data
-//         })
-//   },
-//
-//   getIsAuth: async (): Promise<AuthResponseType> => {
-//     const authPath = '/v1/auth/me'
-//     const res = await fetch(authServices.baseUrl + authPath)
-//     return (res.status === 200)
-//         ? await res.json()
-//         : await res.json()
-//   },
-//
-//   postCreateUser: async (email:string, password:string): Promise<CreateUserResponseType> => {
-//       const signUpPath = '/v1/auth/sign-up'
-//       const response = await fetch(authServices.baseUrl + signUpPath, {
-//         method: 'POST',
-//         headers: {'Content-Type': 'application/json;charset=utf-8'},
-//         body: JSON.stringify({email, password})
-//       });
-//       if (response.status !== 201) {
-//         const errorData = await response.json()
-//         const errorMessage = errorData.errorMessages[0]
-//         throw new Error(errorMessage)
-//       }
-//       return await response.json()
-//     }
-//
-// } as const
 
-
-// const baseUrl = 'https://api.flashcards.andrii.es'
-// const  checkAuthentication = '/v1/auth/me'
-//
-// export function checkAuthenticationWithPromise() {
-//     fetch(baseUrl+checkAuthentication)
-//         .then((res) => {
-//             console.log(res)
-//             if (res.ok) {
-//                 setIsAuthenticated(true)
-//                 return res.json()
-//             } else {
-//                 throw new Error('Вы не авторизованы' + res.statusText )
-//             }
-//         } )
-//         .then((data) => {
-//             console.log('Success', data)
-//         })
-//         .catch((er) => {
-//             console.error('here`s problem', er)
-//         })
-// }
 
 
 
