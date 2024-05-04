a591e5c954904e7acc99193bee229acd50ba088b
Young Eric <erikbonopart12@gmail.com>
2024-05-04 19:07:32 +0300

    -refactor radioGroupComponent

--- src/pages/testPages/testComponentsPage.tsx
+++ src/pages/testPages/testComponentsPage.tsx
@@ -2,7 +2,6 @@ import {RadioGroupComponent} from "@/components/ui/radioGroup";
 import s from './testPage.module.scss'
 import {SelectComponent} from "@/components/ui/select";
 import {TabSwitcher} from "@/components/ui/tabSwitcher";
-import {TextField} from "@/components/ui/textField";
 
 export const TestComponentsPage = () => {
   const tabs = [
@@ -15,9 +14,6 @@ export const TestComponentsPage = () => {
         <RadioGroupComponent/>
         <SelectComponent options={["apple,lemon"]}/>
         <TabSwitcher tabs={tabs} valueName={"fruit"}/>
-        <TextField/>
-        <TextField variant={"password"}/>
-        <TextField variant={"search"}/>
       </div>
   )
 }
