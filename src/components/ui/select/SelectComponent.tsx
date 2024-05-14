a90e83c3505cdca81d7710a9900737c73b837f8b
Young Eric <erikbonopart12@gmail.com>
2024-05-14 20:58:55 +0300

    -refactor SelectComponent by RadixPrimitives

--- src/components/ui/select/SelectComponent.tsx
+++ src/components/ui/select/SelectComponent.tsx
@@ -1,29 +1,77 @@
-
-import s from './select.module.scss'
-import {useState} from "react";
+import React from 'react';
+import * as Select from '@radix-ui/react-select';
+// import classnames from 'classnames';
+import s from './select.module.scss';
+import {SelectDownIcon} from "@/components/assets/icons/componentSvg/selectDownIcon";
+import {SelectUpIcon} from "@/components/assets/icons/componentSvg/selectUpComponent";
 import {Typography} from "@/components/ui/typography";
 
-type SelectPropsType = {
-  options: string[]
-  disabled?: boolean
-}
-export const SelectComponent = (selectProps: SelectPropsType) => {
-  const {options,disabled,...res} = selectProps
-  const [selectedOption, setSelectedOption] = useState<string>(options[0] || '')
 
-  return (
-      <div className={s.select_Container}>
-        <Typography as={"label"} variant={"label"}>{selectedOption}</Typography>
-        <select className={s.select} disabled={!!disabled} {...res} value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
-          {options.map((option, index) => (
-              <option key={index} value={option}>{<Typography variant={'body1'}>{option}</Typography>}</option>
-          ))}
-        </select>
+type SelectComponentProps = {
+  items: SelectItemProps[];
+};
 
+export const SelectComponent = ({items}: SelectComponentProps) => (
+    <Select.Root>
+      <div className={s.rootContainer}>
+      <Select.Trigger className={s.SelectTrigger} aria-label="Food">
+        <Typography><Select.Value placeholder="Select box" /></Typography>
+        <Select.Icon className={s.SelectIcon}>
+         <SelectDownIcon/>
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content className={s.SelectContent}>
+          <Select.ScrollUpButton className={s.SelectScrollButton}>
+            <SelectUpIcon/>
+          </Select.ScrollUpButton>
+          <Select.Viewport className={s.SelectViewport}>
+            <Select.Group>
+              {items.map((item, index) => (
+                  <SelectItem key={index} value={item.value}>
+                    {item.children}
+                  </SelectItem>
+              ))}
+              {/*<SelectItem value="apple">Apple</SelectItem>*/}
+              {/*<SelectItem value="banana">Banana</SelectItem>*/}
+              {/*<SelectItem value="blueberry">Blueberry</SelectItem>*/}
+              {/*<SelectItem value="grapes">Grapes</SelectItem>*/}
+              {/*<SelectItem value="pineapple">Pineapple</SelectItem>*/}
+            </Select.Group>
+
+          </Select.Viewport>
+          <Select.ScrollDownButton className={s.SelectScrollButton}>
+            <SelectDownIcon/>
+          </Select.ScrollDownButton>
+        </Select.Content>
+      </Select.Portal>
       </div>
+    </Select.Root>
+);
 
+type SelectItemProps = {
+  value: string;
+  children: string;
+} & React.ComponentPropsWithoutRef<typeof Select.Item>;
+
+const SelectItem = React.forwardRef<HTMLDivElement ,SelectItemProps>(({ children, className, ...props }, forwardedRef) => {
+  return (
+      <Select.Item className={s.SelectItem} {...props} ref={forwardedRef}>
+        <Select.ItemText>{children}</Select.ItemText>
+        <Select.ItemIndicator className="SelectItemIndicator">
+        </Select.ItemIndicator>
+      </Select.Item>
   );
-}
+});
 
-
+// const SelectItem2 = React.forwardRef<HTMLDivElement ,SelectItemProps>(({ children, className, ...props }, forwardedRef) => {
+//   return (
+//
+//       <Select.Item className={s.SelectItem} {...props} ref={forwardedRef}>
+//         <Typography variant={"body1"}>{children}</Typography>
+//         <Select.ItemIndicator className="SelectItemIndicator">
+//         </Select.ItemIndicator>
+//       </Select.Item>
+//   );
+// });
 
--- src/pages/testPages/testComponentsPage.tsx
+++ src/pages/testPages/testComponentsPage.tsx
@@ -1,19 +1,25 @@
 import {RadioGroupComponent} from "@/components/ui/radioGroup";
 import s from './testPage.module.scss'
 import {SelectComponent} from "@/components/ui/select";
-import {TabSwitcher} from "@/components/ui/tabSwitcher";
+
 
 export const TestComponentsPage = () => {
-  const tabs = [
-    { title: 'Apple' },
-    { title: 'Juice' },
-    { title: 'Lemon' },
-  ]
+  // const tabs = [
+  //   { title: 'Apple' },
+  //   { title: 'Juice' },
+  //   { title: 'Lemon' },
+  // ]
+
   return (
       <div className={s.testContainer}>
         <RadioGroupComponent/>
-        <SelectComponent options={["apple,lemon"]}/>
-        <TabSwitcher tabs={tabs} valueName={"fruit"}/>
+        <SelectComponent  items={[
+          { value: 'apple', children: 'Apple' },
+          { value: 'banana', children: 'Banana' },
+          { value: 'blueberry', children: 'Blueberry' },
+          { value: 'grapes', children: 'Grapes' },
+          { value: 'pineapple', children: 'Pineapple' },
+        ]} />
       </div>
   )
 }
