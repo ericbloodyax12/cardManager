e98b8ab95b10f559ea1f5038162cb6f7e97bbaf5
Young Eric <erikbonopart12@gmail.com>
2024-05-25 18:17:01 +0300

    -add @radix-ui/react-dropdown-menu library -add dropDownMenu component and styles

--- src/pages/testPages/testComponentsPage.tsx
+++ src/pages/testPages/testComponentsPage.tsx
@@ -1,8 +1,7 @@
-import {RadioGroupComponent} from "@/components/ui/radioGroup";
+
 import s from './testPage.module.scss'
-import {SelectComponent} from "@/components/ui/select";
-import {TabSwitcher} from "@/components/ui/tabSwitcher";
-import {SliderComponent} from "@/components/ui/slider";
+
+import {DropDownMenuComponent} from "@/components/ui/dropDownMenu/dropDownMenu";
 
 
 export const TestComponentsPage = () => {
@@ -14,30 +13,7 @@ export const TestComponentsPage = () => {
 
   return (
       <div className={s.testContainer}>
-        <RadioGroupComponent/>
-        <SelectComponent  items={[
-          { value: 'apple', children: 'Apple' },
-          { value: 'banana', children: 'Banana' },
-          { value: 'blueberry', children: 'Blueberry' },
-          { value: 'grapes1', children: 'Grapes' },
-          { value: 'grapes2', children: 'Grapes' },
-          { value: 'grapes3', children: 'Grapes' },
-          { value: 'grapes4', children: 'Grapes' },
-          { value: 'grapes5', children: 'Grapes' },
-          { value: 'grapes6', children: 'Grapes' },
-          { value: 'grapes7', children: 'Grapes' },
-          { value: 'grapes8', children: 'Grapes' },
-          { value: 'apple9', children: 'Apple' },
-          { value: 'pineapple', children: 'Pineapple' }
-        ]} />
-        <TabSwitcher tabs={ [
-          { title: 'Apple' },
-          { title: 'Juice' },
-          { title: 'Juice' },
-          { title: 'Lemon' }
-        ]} valueName="my-tabs" disabled={false}/>
-
-        <SliderComponent/>
+        <DropDownMenuComponent/>
       </div>
 
 
