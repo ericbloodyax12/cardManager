0c1006429291a031f7d3caa075a0f570f4b64868
Young Eric <erikbonopart12@gmail.com>
2024-07-01 18:29:36 +0300

    -refactor -create default route realization

--- src/pages/publicPages/signUp/signUp.tsx
+++ src/pages/publicPages/signUp/signUp.tsx
@@ -5,7 +5,6 @@ import s from './sign-up.module.scss'
 export const SignUp = () => {
     return (
         <div className={s.signUpContainer}>
-
           <SignUpForm />
         </div>
     );
--- src/routing/routerWrapper.tsx
+++ src/routing/routerWrapper.tsx
@@ -3,18 +3,29 @@ import { createBrowserRouter, Navigate,  RouterProvider } from "react-router-dom
 import {MainLayoutWrapper} from "@/pages/mainLayoutWrapper/mainLayoutWrapper";
 
 import {PrivateRouteWrapper} from "@/routing/privateRouteWrapper";
-import {routesConfig, SIGN_IN} from "@/routing/routesList/Routes";
+import {routesConfig, SIGN_IN, SIGN_UP} from "@/routing/routesList/Routes";
+import {Decks} from "@/pages/decks";
+import {SignIn} from "@/pages/publicPages/signIn/signIn";
 export const isAuth = false;
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayoutWrapper isAuth={false}/>,
-    children: routesConfig.map(route => ({
-      path: route.path,
-      handle: route.buttonInfo,
-      element: route.private ? <PrivateRouteWrapper isAuth={isAuth}/> : route.element,
-    })),
+    handle: {buttonText:"SignUp",  navigateTo:  SIGN_UP},
+    element: <MainLayoutWrapper isAuth={isAuth}/>,
+    children: [
+      ...routesConfig.map(route => ({
+        path: route.path,
+        handle: route.buttonInfo,
+        element: route.private ? <PrivateRouteWrapper isAuth={isAuth} /> : route.element,
+      })),
+      // Указываем маршрут по умолчанию
+      {
+
+        index: true,
+        element: isAuth ? <Decks /> : <SignIn/>, // компонент, который будет отображаться по умолчанию
+      }
+    ],
   },
   {
     path: "*",
@@ -24,6 +35,6 @@ const router = createBrowserRouter([
 
 export const RouterWrapper = () => {
   return (
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
   );
 }
