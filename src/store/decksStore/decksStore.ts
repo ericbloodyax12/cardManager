ace3b6a9a5aa3dd559f59115bcd5188e079a760f
Young Eric <erikbonopart12@gmail.com>
2024-09-28 14:02:03 +0300

    - refactor

--- src/store/decksStore/decksStore.ts
+++ src/store/decksStore/decksStore.ts
@@ -77,23 +77,24 @@ export class DecksStore {
     }
 
     async deleteDeck(id: string, bearerToken: string, delitingDeckInfo: DeckModelView): Promise<IDeckBaseModel | undefined> {
-        try {
-            const currentUserData = await this._authService.getCurrentUserData(this._userTokens?.accessToken)
-
-            if(currentUserData.id === delitingDeckInfo.author.id) {
+        const currentUserData = await this._authService.getCurrentUserData(this._userTokens?.accessToken)
+        if (currentUserData.id === delitingDeckInfo.author.id) {
+            try {
                 const deletedDeck = await this._decksService.deleteDeck(id, bearerToken)
                 return deletedDeck
-            } else {
-                throw new Error("You can't delete decks that don't belong to you")
+
+            } catch (e: any) {
+                if (e.response?.status === 403) { //todo исправить сообщение об ошибке
+                    this.error = e.message;
+                } else {
+                    this.error = e.message || 'Something went wrong';
+                }
             }
 
-        } catch (e: any) {
-            if (e.response?.status === 403) { //todo исправить сообщение об ошибке
-                this.error = e.message;
-            } else {
-                this.error = e.message || 'Something went wrong';
-            }
+        } else {
+            throw new Error("You can't delete decks that don't belong to you")
         }
+
     }
 
     async updateDeck(payload: {deckId: string, name?: string, cover?: File | undefined, isPrivate?: boolean, bearerToken?: string}) {
