a4d58cbf11d9ed45af98659e920f1682ad8a62dd
Young Eric <erikbonopart12@gmail.com>
2024-10-05 11:25:15 +0300

    -fix path error in getCardsInDeck request in cards-service and test getCardsInDeck by deckIdTest

--- src/services/api/cards-service/cards-service.ts
+++ src/services/api/cards-service/cards-service.ts
@@ -4,11 +4,11 @@ import {ICardsDataResponse} from "@/dto/cards/cards-dto";
 
 export class CardsService extends ApiService {
     private decksPath: string = "/v1/decks/";
-    private cardsPath: string = "/v1/cards/";
+    private cardsPath: string = "/cards";
 
     async getCardsInDeck(id: string, bearerToken?: string): Promise<ICardsDataResponse> {
         const queryParams = id;
-        const path = `${this.decksPath}`+ queryParams;
+        const path = this.decksPath + queryParams + this.cardsPath;
         const headers: { [key: string]: string } = bearerToken ? { Authorization: `Bearer ${bearerToken}` } : {};
         const res = await super.get<ICardsDataResponse>({
             path: path,
